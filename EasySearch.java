69aeeaff2bc8e51e8d1a4b893dea4873530ab75b
JAY <jmmodi@indiana.edu>
2015-10-22 15:32:40 -0400

    finding relevance score for all queries

--- EasySearch.java
+++ EasySearch.java
@@ -3,34 +3,37 @@ package Assignment2;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.index.LeafReaderContext;
-import org.apache.lucene.index.Term;
+import org.apache.lucene.index.*;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.similarities.DefaultSimilarity;
 import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.BytesRef;
 
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class EasySearch {
 
     public IndexReader reader;
     public IndexSearcher searcher;
     public Analyzer analyzer;
+    public Map<String, Double> documentLengthMap;
+    public Map<String, Integer> termFreqPerDocMap;
+
+    List<LeafReaderContext> leafReaderContexts;
 
     public EasySearch(String path) {
         try {
             this.reader = DirectoryReader.open(FSDirectory.open(Paths.get(path)));
             this.searcher = new IndexSearcher(reader);
             this.analyzer = new StandardAnalyzer();
+            documentLengthMap = new HashMap<String, Double>();
+            termFreqPerDocMap = new HashMap<String, Integer>();
+            leafReaderContexts = reader.getContext().leaves();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -38,45 +41,102 @@ public class EasySearch {
 
 
     public double calculateRelevanceScore(String queryString) {
+        int totalNumberOfDocs = reader.maxDoc();
+        System.out.println("totalNumberOfDocs = " + totalNumberOfDocs);
+        calculateLengthForAllDocs();
+
 
         Set<Term> queryTerms = getQueryTerms(queryString);
         for (Term queryTerm : queryTerms) {
-            int documentFreq = getDocumentFrequency(queryTerm);
-            System.out.println(documentFreq);
+            calculateTermFreqForAllDocs(queryTerm.text());
         }
 
-        int normalizedDocumentLength = getNormalizedDocumentLength();
-
-
-        return 0.0;
-    }
-
-    private int getNormalizedDocumentLength() {
-        DefaultSimilarity defaultSimilarity = new DefaultSimilarity();
-        List<LeafReaderContext> leafReaderContexts = reader.getContext().leaves();
-
         for (LeafReaderContext leafReaderContext : leafReaderContexts) {
-            int startDocNo = leafReaderContext.docBase;
-            int numberOfDoc = leafReaderContext.reader().maxDoc();
-            for (int docId = 0; docId < numberOfDoc; docId++) {
+            int startDoc = leafReaderContext.docBase;
+            int numberDocs = leafReaderContext.reader().maxDoc();
 
-                float normDocLeng = 0;
+
+            for (int docId = 0; docId < numberDocs; docId++) {
                 try {
-                    normDocLeng = defaultSimilarity.decodeNormValue(leafReaderContext.reader().getNormValues("TEXT").get(docId));
-                    // Get length of the document
-                    float docLeng = 1 / (normDocLeng * normDocLeng);
-                    System.out.println("Length of doc(" + (docId +
-                            startDocNo)
-                            + ", " + searcher.doc(docId +
-                            startDocNo).get("DOCNO")
-                            + ") is " + docLeng);
+                    String docNo = searcher.doc(docId + startDoc).get("DOCNO");
+                    double tfIDF = 0.0;
+                    int termFrequencyPerDocument;
+                    for (Term term : queryTerms) {
+                        if (termFreqPerDocMap.containsKey(docNo + term.text())) {
+                            termFrequencyPerDocument = termFreqPerDocMap.get(docNo + term.text());
+                        } else {
+                            termFrequencyPerDocument = 0;
+                        }
+                        double lengthOfDocument = documentLengthMap.get(docNo);
+                        int documentFrequency = getDocumentFrequency(term);
+                        tfIDF += calculateTFIDF(totalNumberOfDocs, termFrequencyPerDocument, lengthOfDocument, documentFrequency);
+                    }
+                    System.out.println("Relevance Score for  " + queryString + " in" + docNo + " is" + tfIDF);
+
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
-        return 0;
+
+
+        return 0.0;
     }
+
+    private void calculateTermFreqForAllDocs(String term) {
+        System.out.println("Making term frequency map for " + term);
+        for (LeafReaderContext leafReaderContext : leafReaderContexts) {
+            try {
+//                int startDoc = leafReaderContext.docBase;
+                PostingsEnum postingsEnum = MultiFields.getTermDocsEnum(leafReaderContext.reader(), "TEXT", new BytesRef(term));
+                int doc;
+                if (postingsEnum != null) {
+                    while ((doc = postingsEnum.nextDoc()) != PostingsEnum.NO_MORE_DOCS) {
+                        System.out.println(postingsEnum.docID());
+                        String docNo = searcher.doc(postingsEnum.docID()).get("DOCNO");
+                        termFreqPerDocMap.put(docNo + term, postingsEnum.freq());
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    private void calculateLengthForAllDocs() {
+        DefaultSimilarity defaultSimilarity = new DefaultSimilarity();
+
+
+        for (LeafReaderContext leafReaderContext : leafReaderContexts) {
+            int startDoc = leafReaderContext.docBase;
+            int numberOfDocs = leafReaderContext.reader().maxDoc();
+
+            double normDocLength;
+
+            for (int docId = 0; docId < numberOfDocs; docId++) {
+                try {
+                    normDocLength = defaultSimilarity.decodeNormValue(leafReaderContext.reader().getNormValues("TEXT").get(docId));
+
+                    double docLength = 1 / (normDocLength * normDocLength);
+
+                    String docNo = searcher.doc(docId + startDoc).get("DOCNO");
+                    documentLengthMap.put(docNo, docLength);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
+
+    public double calculateTFIDF(int totalNumberOfDocuments, int countOfTermPerDocument, double documentLength, int termFrequency) {
+        return ((double) countOfTermPerDocument / documentLength) * Math.log(1 + totalNumberOfDocuments / termFrequency);
+    }
+
 
     private int getDocumentFrequency(Term queryTerm) {
         int freq = 0;
--- MainClass.java
+++ MainClass.java
@@ -5,7 +5,7 @@ public class MainClass {
 
     public static void main(String[] args) {
         String queryString = "New York";
-        String indexPath = "D:\\Jay\\IUB\\Fall_2015\\Search\\Programming Assignment\\Assignment -2\\index\\index";
+        String indexPath = "D:\\Jay\\IUB\\Fall_2015\\Search\\Programming Assignment\\Assignment -2\\index";
 
         EasySearch easySearch = new EasySearch(indexPath);
 
